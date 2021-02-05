2391e28a0f16bfc4745b2284473e589d053b7cc9
Biswajeet-Sahoo <biswajeetsahoo@rarecrew.com>
2021-02-05 11:31:12 +0530

    FIX: Completed task

--- Finance/src/app/app.module.ts
+++ Finance/src/app/app.module.ts
@@ -1,9 +1,15 @@
+
+import { SliderModule } from 'primeng/slider';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { BusinessComponent } from './business/business.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {InputSwitchModule} from 'primeng/inputswitch';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [
@@ -13,7 +19,13 @@ import { BusinessComponent } from './business/business.component';
     BusinessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    SliderModule,
+    InputSwitchModule,
+    InputTextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Finance/src/app/main-content/main-content.component.ts
+++ Finance/src/app/main-content/main-content.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-main-content',
@@ -7,7 +8,32 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 
-export class MainContentComponent {
-
+export class MainContentComponent implements OnInit {
+  constructor(
+    public fb: FormBuilder,
+  ) {}
   
+  formGroup: FormGroup;
+  incomeValue = 0;
+  expenseValue = 0;
+  tenures = [{name: '6 Months', value: '6 Months'}, {name: '12 Months' , value: '12 Months'}, {name: '24 Months' , value: '24 Months'}]
+
+  ngOnInit(): void {
+    this.formInit();
+  }
+  
+  private formInit() : void{
+    this.formGroup = this.fb.group({
+      monthlyIncome: [],
+      monthlyExpense: [],
+      enabled:[true]
+    });
+  }
+
+  handleChange(event){
+    this.incomeValue = event.value;
+  }
+  handleExpenseChange(event){
+    this.expenseValue = event.value;
+  }
 }
